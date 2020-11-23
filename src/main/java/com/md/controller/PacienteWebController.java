1c0310beeb755e8552c47db54031ca980e310d15
Sergio Ramirez <47802275+chechoXR@users.noreply.github.com>
2020-11-23 17:25:54 -0500

    CRUD de paciente web funcionando

--- src/main/java/com/md/controller/PacienteWebController.java
+++ src/main/java/com/md/controller/PacienteWebController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,50 +15,48 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.md.entity.MedicoWeb;
-import com.md.service.MedicoWebService;
+import com.md.entity.PacienteWeb;
+import com.md.service.PacienteWebService;
 
 @RestController
-@RequestMapping("/web/medicos")
-public class MedicoWebController {
+@RequestMapping("/web/pacientes")
+public class PacienteWebController {
 
 	@Autowired
-	MedicoWebService service;
+	PacienteWebService service;
 	
 	@PostMapping
-	public ResponseEntity<MedicoWeb> create(@RequestBody MedicoWeb medico){
-		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(medico));
+	public ResponseEntity<PacienteWeb> create(@RequestBody PacienteWeb paciente){
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(paciente));
 	}
 	
 	@GetMapping()
-	public List<MedicoWeb> findAll(){
-		List<MedicoWeb> medicos = StreamSupport.stream(service.findAll().spliterator(), false)
+	public List<PacienteWeb> findAll(){
+		List<PacienteWeb> pacientes = StreamSupport.stream(service.findAll().spliterator(), false)
 				.collect(Collectors.toList());
-		return medicos;
+		return pacientes;
 	}
 	
 	@PutMapping("{id}")
-	public ResponseEntity<?> update(@PathVariable Long id, @RequestBody MedicoWeb medico){
-		Optional<MedicoWeb> optional = service.findById(id);
+	public ResponseEntity<?> update(@PathVariable Long id, @RequestBody PacienteWeb paciente){
+		Optional<PacienteWeb> optional = service.findById(id);
 		
 		if(!optional.isPresent())
 			return ResponseEntity.notFound().build();
 		
-		optional.get().setNombre(medico.getNombre());
-		optional.get().setIdentificacion(medico.getIdentificacion());
-		optional.get().setTipoIdentificacion(medico.getTipoIdentificacion());
-		optional.get().setNumeroTarjetaProfesional(medico.getNumeroTarjetaProfesional());
-		optional.get().setAniosExperiencia(medico.getAniosExperiencia());
-		optional.get().setEspecialidad(medico.getEspecialidad());
-		optional.get().setInicio(medico.getInicio());
-		optional.get().setFin(medico.getFin());
+		optional.get().setNombre(paciente.getNombre());
+		optional.get().setFechaNacimiento(paciente.getFechaNacimiento());
+		optional.get().setIdentificacion(paciente.getIdentificacion());
+		optional.get().setTipoIdentificacion(paciente.getTipoIdentificacion());
+		optional.get().setEPS(paciente.getEPS());
+		optional.get().setHistoriaClinica(paciente.getHistoriaClinica());
 		
 		return ResponseEntity.status(HttpStatus.OK).body(service.save(optional.get()));
 	}
 	
 	@DeleteMapping("{id}")
 	public ResponseEntity<?> deleteById(@PathVariable Long id){
-		Optional<MedicoWeb> optional = service.findById(id);
+		Optional<PacienteWeb> optional = service.findById(id);
 		
 		if(!optional.isPresent())
 			return ResponseEntity.notFound().build();
@@ -68,5 +65,6 @@ public class MedicoWebController {
 		
 		return ResponseEntity.ok().build();
 	}
+
 	
 }
