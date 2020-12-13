471fd9e7f9bec0daa839a59b63bee35f4bcab66e
Sergio Ramirez <47802275+chechoXR@users.noreply.github.com>
2020-12-12 21:32:00 -0500

    Se agregan cast a queries previas

--- src/main/java/com/md/repository/CitaWebRepository.java
+++ src/main/java/com/md/repository/CitaWebRepository.java
@@ -12,10 +12,10 @@ import com.md.entity.CitaWeb;
 @Repository
 public interface CitaWebRepository extends JpaRepository<CitaWeb, Long>{
 	
-	@Query(value = "SELECT * FROM cita_web WHERE medico_id = ?1 AND inicio = ?2", nativeQuery = true)
+	@Query(value = "SELECT * FROM cita_web WHERE CAST(medico_id AS bigint) = ?1 AND inicio = ?2", nativeQuery = true)
 	public List<CitaWeb> citasDelDiaMedico(Long medicoid, @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)Date inicio);
 
-	@Query(value = "SELECT * FROM cita_web WHERE paciente_id = ?1 AND inicio = ?2", nativeQuery = true)
+	@Query(value = "SELECT * FROM cita_web WHERE CAST(paciente_id AS bigint) = ?1 AND inicio = ?2", nativeQuery = true)
 	public List<CitaWeb> citasDelDiaPaciente(Long medicoid, @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)Date inicio);
 	
 	
