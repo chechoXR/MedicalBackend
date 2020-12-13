d12e28ffebd24aaad3efbf48c8f677db7b1b90e4
Sergio Ramirez <47802275+chechoXR@users.noreply.github.com>
2020-12-12 22:50:39 -0500

    Se arregla Query para retornar citas_web

--- src/main/java/com/md/controller/CitaWebController.java
+++ src/main/java/com/md/controller/CitaWebController.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.StringTokenizer;
 import java.util.TimeZone;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
